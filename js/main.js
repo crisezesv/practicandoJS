b0f778ec17e7356f0eb18a0318db29ed2d9a232f
crisezesv <cristianezequiel.sv@gmail.com>
2023-11-28 23:48:50 -0300

    practicando JS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let producto = prompt ("Ingresa el nombre del producto");
+let cantidad = prompt ("Ingresa la cantidad del producto seleccionado")
+
+
+
+if (cantidad > 5) {
+    alert ("Disculpe, no esta permitido mas de 5 productos");
+    }
+
+else if ( cantidad < 5 ) { 
+    alert("Gracias por su compra, vuelva pronto");
+}
+
+
